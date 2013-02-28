f108459a4015fd729c84d6662733b9b9fa65dbb7
Jared Stehler <jared.stehler@cengage.com>
2013-02-27 22:26:30 -0500

    updated to support providing stealUrl

--- src/template-jasmine-steal.js
+++ src/template-jasmine-steal.js
@@ -7,22 +7,23 @@ var template = __dirname + '/templates/jasmine-steal.html',
     };
 
 exports.process = function(grunt, task, context) {
-  var version = context.options.version;
+  var stealUrl = context.options.stealOptions.stealUrl;
 
   // find the latest version if none given
-  if (!version) {
-    version = Object.keys(steal).sort().pop();
+  if (!stealUrl) {
+    stealUrl = '.grunt/grunt-contrib-jasmine/steal/steal.js';
   }
 
   context.options.stealOptions = context.options.stealOptions || {};
+  context.options.stealOptions.stealUrl = stealUrl;
 
   context.options.stealOptions.config = grunt.util._.extend({
   
   }, context.options.stealOptions.config);
 
   // put steal into place
-  task.copyTempFile(steal[version] + '/steal.js','steal/steal.js');
-  task.copyTempFile(steal[version] + '/dev/dev.js','steal/dev/dev.js');
+  task.copyTempFile(steal['3.2.3'] + '/steal.js','steal/steal.js');
+  task.copyTempFile(steal['3.2.3'] + '/dev/dev.js','steal/dev/dev.js');
 
   var source = grunt.file.read(template);
   return grunt.util._.template(source, context);
